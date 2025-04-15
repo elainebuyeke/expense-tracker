2b755ae6a0ef036f68c3f45a02890c7b3c0901fe
Elaine <omwengaelaine@gmail.com>
2025-04-15 09:50:57 +0300

    added info into form

--- src/data/Initialexpenses.jsx
+++ src/data/Initialexpenses.jsx
@@ -2,20 +2,23 @@ const Initialexpenses = [
     {
         expense:"Buy shoes",
         description:"Add to my shoe collection",
+        category:"Wants",
         amount:"2500",  
-        date:""
+        date:"31/8/2024"
     },
     {
         expense:"Buy book",
         description:"Improve my reading",
+        category:"Wants",
         amount:"1300",
-        date:""
+        date:"2/4/2025"
     },
     {
         expense:"Pay loan",
         description:"Reduce loan payment",
+        category:"Needs",
         amount:"10000",
-        date:""
+        date:"13/4/2025"
     }
    
 ]
